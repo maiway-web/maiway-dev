5b2c20bee6863939bd5e720d50aa8b81b5b2b4dc
celmar <celmargalindez@gmail.com>
2020-10-19 15:54:37 +0800

    Updates

---
+++ precache-manifest.574cdc8f09c2af426ca0942ef1ff31fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a8ceb5e39ccd475e961cbd08f153c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea24e077c892ca8253c",
+    "url": "/static/js/2.891df6ae.chunk.js"
+  },
+  {
+    "revision": "fadca043f8f94465a8e80b6eb4fd9de9",
+    "url": "/static/js/2.891df6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e19322111c4dcb25dd2",
+    "url": "/static/js/main.2883a224.chunk.js"
+  },
+  {
+    "revision": "1fcdb9be3c569d608915",
+    "url": "/static/js/runtime-main.72836c52.js"
+  }
+]);
